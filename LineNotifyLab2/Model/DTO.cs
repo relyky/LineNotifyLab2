0cd1e7ff4113d756335032e3037383a9c0f23ba6
Rely Ky <relyky@hotmail.com>
2024-01-11 15:58:52 +0800

    notifymgmt

--- LineNotifyLab2/Model/DTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LineNotifyLab2.Model;
-
-public class DTO
-{
-}
-
-public record LineNotifyResponse(int status, string message);
-
-public record LineNotifyTokenResponse(int status, string message, string access_token);
---
+++ LineNotifyLab2/Model/LineNotifyData.cs
@@ -0,0 +1,33 @@
+﻿namespace LineNotifyLab2.Model;
+
+public record LineNotifyData
+{
+  /// <summary>
+  /// 聊天室識別名稱: 用此計算 LINE Notify state 參數。比如轉成 base64。
+  /// </summary>
+  public string IdName { get; set; } = string.Empty;
+
+  /// <summary>
+  /// LINE Notify client_id
+  /// </summary>
+  public string ClientID { get; set; } = string.Empty;
+
+  /// <summary>
+  /// LINE Notify client_secret
+  /// </summary>
+  public string ClientSecret { get; set; } = string.Empty;
+
+  /// <summary>
+  /// LINE Notify redirect_uri
+  /// </summary>
+  public string RedirectUri { get; set; } = string.Empty;
+
+  /// <summary>
+  /// 聊天室 access_token
+  /// </summary>
+  public string AccessToken { get; set; } = string.Empty;
+}
+
+public record LineNotifyResponse(int status, string message);
+
+public record LineNotifyTokenResponse(int status, string message, string access_token);
