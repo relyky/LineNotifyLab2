f8da51c54e543a0243e1dffb57c5328e4b73e7bf
Rely Ky <relyky@hotmail.com>
2024-01-11 15:34:19 +0800

    notifyback - 2

---
+++ LineNotifyLab2/Model/DTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LineNotifyLab2.Model;
+
+public class DTO
+{
+}
+
+public record LineNotifyResponse(int status, string message);
+
+public record LineNotifyTokenResponse(int status, string message, string access_token);
