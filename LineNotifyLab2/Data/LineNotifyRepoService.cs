9930ccf73c666acd6a9b3f662fe0d3e94ae77f88
Rely Ky <relyky@hotmail.com>
2024-01-12 11:27:42 +0800

    Line Notify Repo

---
+++ LineNotifyLab2/Data/LineNotifyRepoService.cs
@@ -0,0 +1,19 @@
+﻿using LineNotifyLab2.Model;
+
+namespace LineNotifyLab2.Data;
+
+internal class LineNotifyRepoService
+{
+  LineNotifyData? repo = null;
+
+  public Task<LineNotifyData?> GetAsync()
+  {
+    return Task.FromResult(repo);
+  }
+
+  public Task SetAsync(LineNotifyData info)
+  {
+    repo = info;
+    return Task.CompletedTask;
+  }
+}
--- LineNotifyLab2/Program.cs
+++ LineNotifyLab2/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<LineNotifyRepoService>();
 
 var app = builder.Build();
 
